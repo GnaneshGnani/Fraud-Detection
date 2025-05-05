5401b62096449a433d98d2228ead7d99533cccf7
GnaneshGnani <gnanesh.dometti@gmail.com>
2025-05-05 14:52:47 -0400

    Minor Changes

--- scripts/detect_drift.py
+++ scripts/detect_drift.py
@@ -4,21 +4,19 @@ import pandas as pd
 from evidently import Report
 from evidently.presets import DataDriftPreset
 
-# ref_df = pd.read_csv("data/baseline.csv").drop(columns = ["id"])
-# cur_df = pd.read_csv("data/current_batch.csv").drop(columns = ["id"])
+ref_df = pd.read_csv("data/baseline.csv").drop(columns = ["id"])
+cur_df = pd.read_csv("data/current_batch.csv").drop(columns = ["id"])
 
-# report = Report(metrics = [DataDriftPreset()])
-# result = report.run(reference_data = ref_df, current_data = cur_df)
+report = Report(metrics = [DataDriftPreset()])
+result = report.run(reference_data = ref_df, current_data = cur_df)
 
-# drift_score = result.dict()['metrics'][0]['value']['share']
+drift_score = result.dict()['metrics'][0]['value']['share']
 
-# today_date = datetime.datetime.today().strftime('%Y-%m-%d')
-# result.save_json(f"reports/drift/{today_date}.json")
+today_date = datetime.datetime.today().strftime('%Y-%m-%d')
+result.save_json(f"reports/drift/{today_date}.json")
 
-# print("Detected Drift:", drift_score)
-# value = "true" if drift_score > 0.5 else "false"
-
-value = "false"
+print("Detected Drift:", drift_score)
+value = "true" if drift_score > 0.5 else "false"
 
 output_path = os.environ["GITHUB_OUTPUT"]
 with open(output_path, "a") as f:
--- scripts/merge_incoming_files.py
+++ scripts/merge_incoming_files.py
@@ -4,7 +4,11 @@ import pandas as pd
 
 files = glob.glob("data/incoming/*.csv")
 
-concatenated_files = pd.concat(map(pd.read_csv, files))
-concatenated_files.to_csv("data/current_batch.csv", index = False)
+if files:
+    concatenated_files = pd.concat(map(pd.read_csv, files))
+    concatenated_files.to_csv("data/current_batch.csv", index = False)
 
-print("Merged Incoming Files")
+    print("Merged Incoming Files")
+
+else:
+    print("No New Files!")
